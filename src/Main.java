f1a9077a800bbbd1befeef4d762e5f7e61e30420
beeland2 <slm05060000@gmail.com>
2025-10-01 19:53:20 +0200

    Descripción de lo que cambiaste

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,89 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Pedir el nombre de la araña
+
+
+
+        String art = """
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡖⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠘⢦⠀⠀⠀⠀⠀⠀⠀⠀⢠⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠈⢳⡀⠀⠀⠀⠀⠀⣠⣾⠙⢦⣀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠔⠂
+⠀⠀⠀⠀⠀⠀⠀⢸⡿⣗⡲⠶⠖⠋⣡⣯⡀⠀⠈⠉⠓⠒⠲⢶⣶⡖⠋⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡼⠁⡿⢯⠙⠛⠋⣹⡇⠙⠲⢤⣀⣀⡤⠖⢫⠏⠀⠀⠀⠀⠀
+⠈⠙⠓⠶⢤⣴⣋⢀⣰⠃⠈⣿⡛⠉⣽⠙⠲⢤⡤⠞⢻⠀⢀⡏⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⢯⡙⢳⡲⢴⣇⣙⣄⣇⡤⠚⠉⡇⠀⢸⠀⢸⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣷⠀⣧⠀⣇⡼⢻⢿⡲⠤⣄⣧⠀⠸⡆⠈⣧⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢠⡇⣠⣿⡊⠙⢦⡞⠀⠳⣴⠋⠉⢉⡷⠿⠤⣌⣦⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⢾⣋⠁⠀⠙⢦⢸⡟⠉⠉⠙⣆⢠⠏⣠⠖⠋⠉⠉⠉⠓⠲⠤⡄
+⠀⠀⣠⠖⠋⠁⠀⠈⠙⠦⡀⠈⣿⣠⠤⠴⠶⠾⢿⣠⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⡏⠀⠀⢀⣀⣀⣀⣻⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡷⠚⠉⠉⠁⠀⠀⠀⠙⣆⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+""";
+
+        int distanciaSubida = 0;
+        int distanciaBajada = 0;
+        boolean comida = false;
+        boolean hizoamigos  = false;
+
+        System.out.print("\nBienvenido a la aventura de la araña 🐜");
+        System.out.print("\nDinos cómo se llama tu araña: ");
+        String nombre = sc.nextLine();
+        System.out.println(art);
+
+        System.out.println("¡Hola, " + nombre + "! Hoy te despiertas en el aula de ASIR 1.");
+        System.out.println("Frente a ti hay una ventana abierta y la puerta del aula.");
+        System.out.println("");
+
+        // Primera decisión
+        System.out.println("Decisión 1: ¿Qué quieres hacer?");
+        System.out.println("1) Entrar al aula");
+        System.out.println("2) Quedarte en la ventana");
+        System.out.println("3) Subir por la cortina");
+
+        System.out.print("> ");
+        int pregunta1 = sc.nextInt();
+
+        // Mostrar elección
+        if(pregunta1 == 1) {
+            System.out.println("\nHas elegido 1: Entrar al aula");
+            // decisión tipo 1.1
+            System.out.println("\nDecisión 2: ¿Qué quieres hacer ahora?");
+            System.out.println("1) Esconderse bajo la mesa");
+            System.out.println("2) Caminar por la pizarra");
+            System.out.println("3) Subirse al hombro de un estudiante");
+
+            System.out.print("> ");
+            int pregunta2 = sc.nextInt();
+            if(pregunta2 == 1) {
+                System.out.println("\nTe escondes bajo la mesa.");
+                System.out.println("Final feliz: Encuentras migas de pan \uD83E\uDD56");
+            } else if(pregunta2 == 2) {
+                System.out.println("\nCaminas por la pizarra.");
+                System.out.println("Final trágico: Profesor te aplasta \uD83D\uDC80");
+            } else if(pregunta2 == 3) {
+                System.out.println("\nTe subes al hombro de un estudiante.");
+                System.out.println("Final imprevisto: Un estudiante te lanza por la ventana \uD83C\uDF2C\uFE0F");
+            } else {
+                System.out.println("\nOpción no válida, sigues explorando el aula.");
+            }
+
+
+        } else if(pregunta1 == 2) {
+            System.out.println("\nHas elegido 2: Quedarte en la ventana");
+
+        } else if (pregunta1 == 3) {
+            System.out.println("\nHas elegido 3: Subir por la cortina");
+        } else {
+            System.out.println("\nOpción no válida, pero continuamos la aventura...");
+        }
+
+
+        sc.close();
     }
 }
